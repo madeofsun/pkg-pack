a6e06130cc6ae614700aad746e65fdf185454a9e
madeofsun <rettou74@gmail.com>
2025-03-29 19:07:58 +0300

    wip

--- packages/interop/src/get-esm-default.ts
+++ packages/interop/src/get-esm-default.ts
@@ -1,19 +1,14 @@
-export function getEsmDefault<
-  T extends object | { default: unknown } | { default: { default: unknown } }
->(object: T) {
-  return (
-    !("default" in object)
-      ? object
-      : !(
-          typeof object.default === "object" &&
-          object.default !== null &&
-          "default" in object.default
-        )
-      ? object.default
-      : object.default.default
-  ) as T extends { default: { default: unknown } }
-    ? T["default"]["default"]
-    : T extends { default: unknown }
+type CheckDoubleDefault<T extends object | { default: unknown }> = T extends {
+  default: { default: unknown };
+}
+  ? "Custom Type Error: This helper cannot be used safely here."
+  : T;
+
+export function getEsmDefault<T extends object | { default: unknown }>(
+  object: CheckDoubleDefault<T>
+) {
+  const o = object as T;
+  return ("default" in o ? o.default : o) as T extends { default: unknown }
     ? T["default"]
     : T;
 }
